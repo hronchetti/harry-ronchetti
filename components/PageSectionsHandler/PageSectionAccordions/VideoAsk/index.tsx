96bb5fce818610d5a9d27ca6a635a3fa78b39a53
hronchetti <harryronchetti@gmail.com>
2023-10-06 18:33:30 +0100

    Updating video ask

--- components/PageSectionsHandler/PageSectionAccordions/VideoAsk/index.tsx
+++ components/PageSectionsHandler/PageSectionAccordions/VideoAsk/index.tsx
@@ -9,7 +9,7 @@ export const VideoAsk = () => (
     <iframe
       title="Tell me about your SaaS Product"
       className="rounded-3xl max-w-[576px] mx-auto z-20 md:absolute md:-translate-y-1/2 md:top-1/2 md:left-1/2 md:-translate-x-1/2 shadow-[0_8px_48px_-12px_rgba(18,25,38,0.16)] bg-white"
-      src="https://www.videoask.com/fprsym165"
+      src="https://www.videoask.com/fdxpb5f8f"
       allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *;"
       width="100%"
       height="400px"
