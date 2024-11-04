a18079916085d2169b6cbaee2a83905ad30245e1
hronchetti <harryronchetti@gmail.com>
2024-11-04 15:41:40 +0000

    more tiny style updates

--- components/HeaderLanding/index.tsx
+++ components/HeaderLanding/index.tsx
@@ -5,7 +5,7 @@ export const HeaderLanding = ({ heading, description }: PageSectionsHeader) => (
   <header className="w-full text-center">
     <div className="relative overflow-hidden">
       <div className="wrapper-x flex flex-wrap justify-center pb-10 pt-6 md:pb-12 md:pt-12 lg:pb-20 lg:pt-16 xl:pb-24">
-        <span className="mb-5 block rounded-lg border border-grey-30 bg-grey-20 px-2 py-1 text-sm font-medium uppercase tracking-widest text-grey-70 sm:mb-6 xl:mb-7">
+        <span className="mb-5 block rounded-lg border border-grey-30 bg-grey-20 px-2 py-1 text-sm font-medium uppercase tracking-widest text-grey-70 sm:mb-6 xl:mb-7 xl:text-[15px]">
           Looking for a new role
         </span>
         <div className="flex w-full justify-center">
@@ -20,7 +20,7 @@ export const HeaderLanding = ({ heading, description }: PageSectionsHeader) => (
             </p>
           </div>
         )}
-        <div className="mt-5 lg:mt-8">
+        <div className="mt-5 lg:mt-8 xl:mt-9">
           <ButtonLink
             href=""
             iconLeft={
